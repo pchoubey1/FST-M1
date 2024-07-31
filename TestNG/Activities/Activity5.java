1cf2602de0814fedf2406dfb54431e13a07c6a52
pchoubey1 <priya.choubey@ibm.com>
2024-07-31 17:57:49 +0530

    Add files via upload

--- TestNG/Activities/Activity5.java
+++ TestNG/Activities/Activity5.java
@@ -15,40 +15,56 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Activity5 {
-	 WebDriver driver;
-	 
-	    @BeforeClass
-	    public void beforeClass() {
-	        // Set up the Firefox driver
-	        WebDriverManager.firefoxdriver().setup();
-	        driver = new FirefoxDriver();
-	 
-	        //Open browser
-	        driver.get("https://v1.training-support.net/selenium/login-form");
-	    }
-	 
-	    @Test
-	    public void loginTest() {
-	        //Find the username and password fields
-	        WebElement username = driver.findElement(By.id("username"));
-	        WebElement password = driver.findElement(By.id("password"));
-	        
-	        //Enter credentials
-	        username.sendKeys("admin");
-	        password.sendKeys("password");
-	        
-	        //Click login
-	        driver.findElement(By.xpath("//button[text()='Log in']")).click();
-	        
-	        //Read login message
-	        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
-	        Assert.assertEquals("Welcome Back, admin", loginMessage);
-	    }
-	 
-	    @AfterClass
-	    public void afterClass() {
-	        //Close browser
-	        driver.close();
-	    }
+	WebDriver driver;
+    //Include alwaysRun property on the @BeforeTest
+    //to make sure the page opens
+    @BeforeTest(alwaysRun = true)
+    public void beforeMethod() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+            
+        //Open the browser
+        driver.get("https://v1.training-support.net/selenium/target-practice");
+    }
+    
+    @Test (groups = {"HeaderTests", "ButtonTests"})
+    public void pageTitleTest() {
+        String title = driver.getTitle();
+        System.out.println("Title is: " + title);
+        Assert.assertEquals(title, "Target Practice");
+    }
+    
+    @Test (dependsOnMethods = {"pageTitleTest"}, groups = {"HeaderTests"})
+    public void HeaderTest1() {
+        WebElement header3 = driver.findElement(By.cssSelector("h3#third-header"));
+        Assert.assertEquals(header3.getText(), "Third header");
+    }
+    
+    @Test (dependsOnMethods = {"pageTitleTest"}, groups = {"HeaderTests"})
+    public void HeaderTest2() {
+        WebElement header5 = driver.findElement(By.cssSelector("h3#third-header"));
+        Assert.assertEquals(header5.getCssValue("color"), "rgb(251, 189, 8)");
+    }
+    
+    @Test (dependsOnMethods = {"pageTitleTest"}, groups = {"ButtonTests"})
+    public void ButtonTest1() {
+        WebElement button1 = driver.findElement(By.cssSelector("button.olive"));
+        Assert.assertEquals(button1.getText(), "Olive");
+    }
+    
+    @Test (dependsOnMethods = {"pageTitleTest"}, groups = {"ButtonTests"})
+    public void ButtonTest2() {
+        WebElement button2 = driver.findElement(By.cssSelector("button.brown"));
+        Assert.assertEquals(button2.getCssValue("color"), "rgb(255, 255, 255)");
+    }
  
+    //Include alwaysRun property on the @AfterTest
+    //to make sure the page closes
+    @AfterTest(alwaysRun = true)
+    public void afterMethod() {
+        //Close the browser
+        driver.close();
+    }
 }
